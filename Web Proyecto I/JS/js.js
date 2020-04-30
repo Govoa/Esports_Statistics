ef5087c50b5b02d7edf1727ad89cd5384db8c37f
Govoa <inigo.vazque@gmail.com>
2020-05-01 00:57:44 +0200

    jqueryadded

---
+++ Web Proyecto I/JS/js.js
@@ -0,0 +1,31 @@
+
+$(document).ready(function()
+{
+    $("#my-table-solos").hide();
+    $("#option1").click(function()
+    {
+        $("#my-table-solos").show();
+        return false;           
+    });
+
+});
+$(document).ready(function()
+{
+    $("#my-table-duos").hide();
+    $("#option2").click(function()
+    {
+        $("#my-table-duos").show();
+        return false;           
+    });
+
+});
+$(document).ready(function()
+{
+    $("#my-table-creativo").hide();
+    $("#option3").click(function()
+    {
+        $("#my-table-creativo").show();
+        return false;           
+    });
+
+});
