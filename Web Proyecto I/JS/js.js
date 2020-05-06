6c438b1b042dc4d9b503b2da906c45f8c8ea31db
Govoa <inigo.vazque@gmail.com>
2020-05-06 02:49:58 +0200

    buscadorbetamuybetamuyfrecodemasiado

--- Web Proyecto I/JS/js.js
+++ Web Proyecto I/JS/js.js
@@ -1,6 +1,9 @@
 
 
 $(document).ready(function(){
+
+
+ 
       $("#my-table-creativo").hide();
       $("#my-table-solos").hide();
       $("#my-table-duos").hide();
@@ -9,15 +12,49 @@ $(document).ready(function(){
       $("#my-table-duos").show();
       $("#my-table-creativo").hide();
       $("#my-table-solos").hide();
+      $("#my-table-solos-q").hide();
+      $("#my-table-creativo-q").hide();
+      $("#my-table-duos-q").hide();
+
     });
     $("#option2").click(function(){
         $("#my-table-creativo").show();
         $("#my-table-solos").hide();
         $("#my-table-duos").hide();
+        $("#my-table-solos-q").hide();
+        $("#my-table-creativo-q").hide();
+        $("#my-table-duos-q").hide();
     });
     $("#option3").click(function(){
         $("#my-table-solos").show();
         $("#my-table-creativo").hide();
         $("#my-table-duos").hide();
+
+        $("#my-table-solos-q").hide();
+      $("#my-table-creativo-q").hide();
+      $("#my-table-duos-q").hide();
     });
+    
+    $("#buscador1").click(function(){
+      $("#my-table-solos").hide();
+      $("#my-table-creativo").hide();
+      $("#my-table-duos").hide();
+      var v = document.getElementById("buscador").value;
+      $("#my-table-creativo-q").sheetrock({
+        url: "https://docs.google.com/spreadsheets/d/1bAvzXFOSQXRAzUZEHxmeQUxhQinHUJTh0xJ5mW" +
+                "wHrl0/edit#gid=0",
+                sql: "select * where B like '" + v + "%'"
+    });
+    $("#my-table-solos-q").sheetrock({
+        url: "https://docs.google.com/spreadsheets/d/1XyTkbMzVKmLEUXx-J8B4QsbRp_3t1TfcJugUvj" +
+                "WLCQg/edit#gid=0",
+                sql: "select * where B like '" + v + "%'"
+    });
+    $("#my-table-duos-q").sheetrock({
+        url: "https://docs.google.com/spreadsheets/d/1CPCVUbsZMw6eW9ntqom3GRR5ZqgnDj0OQnzzas" +
+                "Xnsx4/edit#gid=0",
+                sql: "select * where B like '" + v + "%'"
+    });
+  });
+
   });
